de9d7483ab87e5a76078b9f64121ac74d7ac921e
Pariwesh <tamrpariwesh@gmail.com>
2022-01-24 22:39:39 +1100

    product page added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,31 @@
-import data from './data'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Sports Store
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="cart-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
+    <Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Sports House
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:id" element={<ProductPage />}></Route>
+            <Route exact path="/" element={<HomePage />}></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import data from '../data'
+import Product from '../components/Product'
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="row center">
+        {data.products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default HomePage
